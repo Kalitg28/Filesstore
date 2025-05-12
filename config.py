e72c2f1c29b66bf7db091d5658ac5bc16b81c015
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-05-12 20:44:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ APP_ID = int(os.environ.get("APP_ID", "27823209")) #Your API ID from my.telegram
 API_HASH = os.environ.get("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002327045567")) #Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002656116998")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "Indian_MV_Admin_Bot") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "6004928770")) # Owner id
 #--------------------------------------------
