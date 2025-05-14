9d93772415405fcc58d77c62e92dbaa631b5d7f5
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-05-14 21:40:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -69,12 +69,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             caption=(
                 f"👋 {query.from_user.username}\n\n"
                 f"🎖️ Available Plans :\n\n"
-                f"● {PRICE1}  For 0 Days Prime Membership\n\n"
+                f"● {PRICE1}  For 7 Days Prime Membership\n\n"
                 f"● {PRICE2}  For 1 Month Prime Membership\n\n"
                 f"● {PRICE3}  For 3 Months Prime Membership\n\n"
                 f"● {PRICE4}  For 6 Months Prime Membership\n\n"
                 f"● {PRICE5}  For 1 Year Prime Membership\n\n\n"
-                f"💵 ASK UPI ID TO ADMIN AND PAY THERE -  <code>{UPI_ID}</code>\n\n\n"
+                f"💵 ᴜᴘɪ ɪᴅ -  -  <code>{UPI_ID}</code>\n\n\n"
                 f"♻️ After Payment You Will Get Instant Membership \n\n\n"
                 f"‼️ Must Send Screenshot after payment & If anyone want custom time membrship then ask admin"
             ),
@@ -82,7 +82,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton(
-                            "ADMIN 24/7", url=(SCREENSHOT_URL)
+                            "ADMIN", url=(SCREENSHOT_URL)
                         )
                     ],
                     [InlineKeyboardButton("🔒 Close", callback_data="close")],
